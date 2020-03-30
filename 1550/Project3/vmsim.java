8a437f0a8068805726617271184b1fd7ca744b54
Nick Zullo <njz12@pitt.edu>
2020-03-30 19:49:19 -0400

    Add SCA

--- 1550/Project3/vmsim.java
+++ 1550/Project3/vmsim.java
@@ -4,15 +4,10 @@ import java.util.*;
 public class vmsim {
 /**
  *   TODO:
- *      Make a RAM/Page Table
  *      HashMap for optimized lookups with OPT
- *          struct for each address? Hold line number that page is used
- *          One pass through input to find and record all used pages, store in a struct, store structs in a hashmap
- *          How tf to make a hashmap in C??
  *      Write OPT
- *      Write LRU
- *      Write SCA
- *      Possible actions - Page hit, Fault no evict, Fault evict clean, Fault evict dirty
+ *      When to write dirty pages that do not get evicted??
+ *      Confirm output numbers are correct?
  */
 
     static Scanner scan;
@@ -53,8 +48,8 @@ public class vmsim {
             long address = Long.decode(scan.next());
             address = address >> 12;
             address = address & 0x000fffff;
-
             boolean hit = false;
+
             for (int i = 0; i < pages.size(); i++){
                 if (address == pages.get(i).address){
                     hit = true;
@@ -72,7 +67,8 @@ public class vmsim {
             if (pages.size() < framenum){
           //      System.out.println("Page Fault adding new page");
                 pages.addFirst(new Page(address));
-            } else if (pages.size() == framenum){         
+            } else if (pages.size() == framenum){  
+                pages.addFirst(new Page(address));   
                 Page holder = pages.removeLast();
             //    System.out.println("Evicting page address " + holder.address);
                 if (holder.dirty){
@@ -84,18 +80,83 @@ public class vmsim {
             }
 
         }  
-      
+       
 
     }
 
     public static void OPT(){
+        
+        while (scan.hasNext()){
+            accesses++;
+            char mode = scan.next().charAt(0);
+            long address = Long.decode(scan.next());
+            address = address >> 12;
+            address = address & 0x000fffff;
+            boolean hit = false;
 
 
+
+
+
+        }
 
 
     }
     
     public static void SCU(){
+        LinkedList<Page> pages = new LinkedList<Page>();
+        while (scan.hasNext()){
+            accesses++;
+            char mode = scan.next().charAt(0);
+            long address = Long.decode(scan.next());
+            address = address >> 12;
+            address = address & 0x000fffff;
+            boolean hit = false;
+
+            for (int i = 0; i < pages.size(); i++){
+                if (address == pages.get(i).address){
+                    hit = true;
+           //         System.out.println("PAGE HIT!");
+                    if (mode == 's'){
+                        pages.get(i).dirty = true;
+                    }
+                    pages.get(i).second = true;
+                }
+            }
+
+            if (hit){
+                continue;
+            }
+            pagefaults++;
+            if (pages.size() < framenum){
+          //      System.out.println("Page Fault adding new page");
+                pages.add(new Page(address));
+            } else if (pages.size() == framenum){    
+                Page holder = null;     
+                for (int i = 0; i < pages.size(); i++){
+                    if (pages.get(i).second == true)
+                        pages.get(i).second = false;
+                    else 
+                        holder = pages.remove(i);
+                }
+                if (holder == null){
+                    for (int i = 0; i < pages.size(); i++){
+                        if (pages.get(i).second == false)
+                            holder = pages.remove(i);
+                    }
+                }
+            //    System.out.println("Evicting page address " + holder.address);
+                if (holder.dirty){
+                    writes++;
+                }
+                pages.add(new Page(address));
+            } else {
+                System.out.println("ERROR MORE FRAMES USED THAN GIVEN");
+                System.exit(0);
+            }
+
+
+        }
 
 
 

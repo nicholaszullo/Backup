fd2bce7791afa8727bb3813ad6072443a067616d
Nick Zullo <njz12@pitt.edu>
2020-07-23 21:51:22 -0400

    Second Solution

--- Leetcode/LongestPrefix.java
+++ Leetcode/LongestPrefix.java
@@ -1,36 +1,38 @@
 /**
  * Runtime O(N*M) where N is number of characters in first word and M is the number of words
- * Memory O(1)s
+ * Memory O(1)
+ * 
+ * Now starts searching from end of string backwards, faster empirical runtime but not fastest
  */
 
 public class LongestPrefix {
 	public static void main(String[] args) {
-		String[] strs = {"aaa", "aaaa", "aaa"};
+		String[] strs = {"a"};
 		System.out.println(longestCommonPrefix(strs));
 	}
 	public static String longestCommonPrefix(String[] strs) {
-		String solution = "";
-        if (strs.length == 0)
-            return solution;
-        for (int place = 0; place < strs[0].length(); place++){
-            char curr;
-            try {
-                curr = strs[0].charAt(place);
-            } catch (IndexOutOfBoundsException e){
-                return solution;
-            }
-            
-            for (int word = 1; word < strs.length; word++){
-                try {
-                    if (strs[word].charAt(place) != curr){
-                        return solution;
-                    }
-                } catch (IndexOutOfBoundsException e){
-                    return solution;
-                }
-            }
-            solution += curr;
+		if (strs.length == 0)
+			return "";
+		for (int currPre = strs[0].length(); currPre > 0; currPre-- ){
+			String curr = strs[0].substring(0, currPre);
+			boolean finished = true;
+			for (int word = 1; word < strs.length; word++){
+				try {
+					if (!strs[word].substring(0, currPre).equals(curr)){
+						finished = false;
+						break;
+					}
+				}
+				catch (IndexOutOfBoundsException e){
+					finished = false;
+					break;
+				}
+			}
+			if (finished){
+				return curr;
+			}
+			
 		}
-		return solution;
+		return "";
     }
 }

cbd3796a98c6ed0cf8c8cc41b222e8f03f0e1ae1
Nick Zullo <njz12@pitt.edu>
2020-07-26 15:09:36 -0400

    Solution

--- Leetcode/LongestSubstring.java
+++ Leetcode/LongestSubstring.java
@@ -1,28 +1,26 @@
 /**
- * Runtime O(N^2) worst case (all unique)	Slightly faster than before but still slow
- * Memory O(N) 
+ * Runtime O(N)
+ * Memory O(R)	where R is the radix of the alphabet 
  */
-import java.util.HashSet;
 
 public class LongestSubstring {
 	public static void main(String[] args) {
-		System.out.println(lengthOfLongestSubstring("abcccdefg") + " expected 5");
+		//System.out.println(lengthOfLongestSubstring("sfcgvhbjknhfdtrfyuglh;ijklnbvfaaasd") + " expected 13");
+		System.out.println(lengthOfLongestSubstring("abcabcbb") + " expected 3");
 	}
 	public static int lengthOfLongestSubstring(String s) {
+		int[] lastSeen = new int[128];				//ASCII size to hold lowercase, uppercase, numbers, space 
+		int start = 0;
 		int longest = 0;
-		HashSet<Character> seen;
-		for (int i = 0; i < s.length()-longest; i++){
-			seen = new HashSet<Character>();
-			int currLength =0; 
-			for (int j = i; j < s.length(); j++){
-				if (seen.add(s.charAt(j))){
-					currLength++;
-				} else {
-					break;
-				}
+		for (int i = 0; i < s.length(); i++){
+			System.out.println(s.charAt(i) + " " + lastSeen[s.charAt(i)] + " " + start + " " + longest);
+			if (lastSeen[s.charAt(i)] > start){				//If last seen is < start, not a duplicate. If seen is > start, that char has been seen in the currently tracked substring
+				start = lastSeen[s.charAt(i)];				//Move to duplicate character but passed first duplicate. Trick is s is still 0 indexed, so not +1 to move passed
 			}
-			if (longest < currLength)
-				longest = currLength;
+			lastSeen[s.charAt(i)] = i+1;					//+1 so not 0 indexed, start is 0 but needs to know 'a' at 3 is not the first one because array defaults to 0(ex abcabcbb)
+			if (longest < (i - start) +1)					//Length is not 0 indexed so +1 to start count at 1
+				longest = (i-start) +1;
+			System.out.println("   " + s.charAt(i) + " " + lastSeen[s.charAt(i)] + " " + start + " " + longest);
 		}
 		return longest;
 	}

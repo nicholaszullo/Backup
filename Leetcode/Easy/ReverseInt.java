507de9fa7c8117427ac8de7509b89dca98b53cca
Nick Zullo <njz12@pitt.edu>
2020-07-31 12:13:55 -0400

    Solution

--- Leetcode/Easy/ReverseInt.java
+++ Leetcode/Easy/ReverseInt.java
@@ -1,28 +1,25 @@
-/**	Build a string of the integer in reverse, then convert to an int. Kinda slow
- * 	Runtime O(N+toString+parseInt)
- * 	Memory O(N)
+/**	Used string for easy overflow protection but too slow. Improve this with more overflow checks
+ * 	Runtime O(N)
+ * 	Memory O(1)
  * 
  */
 
 public class ReverseInt{
 
 	public static void main(String[] args) {
-		System.out.println(reverse(1999999999));
+		System.out.println(reverse(-171471289) + " expected " + -982174171);
 	}
 	public static int reverse(int x){
-		boolean negative = x < 0 ? true : false;
-		StringBuilder reversed = new StringBuilder();
+		int answer = 0;
+		int end = 0;										//Check if first digit of x is equal to last digit of answer. not the most robust overflow protection, but decent
 		while (x != 0){
-			reversed.append(Math.abs(x%10));
+			if (x / 10 == 0)
+				end = x % 10;
+			answer *= 10;									//*10 first so first digit is in 1s place not 10s place
+			answer += x % 10;
 			x /= 10;
 		}
-		try{
-			int answer = Integer.parseInt(reversed.toString());
-			return negative ? -answer : answer;
-		} catch (Exception NumberFormatException){
-			return 0;
-		}
-		
+		return end != answer % 10 ? 0 : answer;				//Return 0 if digits dont match, answer otherwise
 	}
 
 }

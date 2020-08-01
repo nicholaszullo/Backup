6a83f8de4bfa0725207e237a5edae0858e72ca2a
Nick Zullo <njz12@pitt.edu>
2020-08-01 14:01:37 -0400

    Solution

--- Leetcode/Easy/MaxSubArray.java
+++ Leetcode/Easy/MaxSubArray.java
@@ -1,22 +1,19 @@
 /**
- * Runtime O(N^2)
+ * Runtime O(N)
  * Memory O(1)
  */
 public class MaxSubArray{
 
 	public static void main(String[] args) {
-		int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
-		System.out.println(maxSubArray(arr) + " expected 6");
+		int[] arr = {4,-5,6,1,-2,-100,1};
+		System.out.println(maxSubArray(arr) + " expected 7");
 	}
 	public static int maxSubArray(int[] nums){
-		int sum = 0, largest = Integer.MIN_VALUE;
-		for (int i = 0; i < nums.length; i++){
-			sum = 0;
-			for (int j = i; j < nums.length; j++){
-				sum += nums[j];
-				largest = sum > largest ? sum : largest;
-			}
+		int sum = 0, max = Integer.MIN_VALUE;
+		for (int i=0;i<nums.length;++i){
+			sum= Math.max(sum + nums[i],nums[i]);				//Continue following this subarray, or reset sum to be this index
+			max=Math.max(max, sum);								//Make sum max if it is the max
 		}
-		return largest;
+		return max;
 	}
 }

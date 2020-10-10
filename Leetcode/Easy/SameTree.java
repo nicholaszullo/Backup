3e8e8b041aaf6ab87fbf63d12e9c0e9332220f84
Nick Zullo <njz12@pitt.edu>
2020-10-09 23:59:12 -0400

    Solution

---
+++ Leetcode/Easy/SameTree.java
@@ -0,0 +1,16 @@
+public class SameTree{
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return dfs(p,q);
+    }
+    private boolean dfs(TreeNode currp, TreeNode currq){
+        if (currp == null && currq == null){
+          return true;
+        } else if (currp == null || currq == null){
+            return false;
+        } else if ((currp != null && currq != null) && currp.val != currq.val){
+            return false;
+        } else {
+            return dfs(currp.left,currq.left) && dfs(currp.right, currq.right); 
+        }
+    }
+}

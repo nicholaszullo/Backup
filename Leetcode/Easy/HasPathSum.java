0107c2c628eaf3f443631159bc8ccb1fab1d83f3
Nick Zullo <njz12@pitt.edu>
2020-10-16 00:07:11 -0400

    Not Needed

--- Leetcode/Easy/HasPathSum.java
+++ Leetcode/Easy/HasPathSum.java
@@ -1,7 +1,6 @@
 public class HasPathSum {
 
     public boolean hasPathSum(TreeNode root, int sum) {
-        if (root==null || (root.val ==sum && (root.left != null || root.right != null))) return false;		//Messy account for special cases. Clean up?
         return dfs(root,sum,0);
     }
     

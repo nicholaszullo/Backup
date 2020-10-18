8cc718e7790182abf07fd9e3cd1c3ab7d29dcb83
Nick Zullo <njz12@pitt.edu>
2020-10-17 22:02:37 -0400

    Solution

---
+++ Leetcode/Easy/ContainsDuplicate.java
@@ -0,0 +1,14 @@
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++){
+            if (!seen.add(nums[i])){
+                return true;
+            }
+            
+        }
+        return false;
+    }
+
+
+}

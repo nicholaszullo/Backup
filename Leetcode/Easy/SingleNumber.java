b1bb0e8fd53001797fe763578bebdcad4caba690
Nick Zullo <njz12@pitt.edu>
2020-10-17 21:47:52 -0400

    Initial Solution

---
+++ Leetcode/Easy/SingleNumber.java
@@ -0,0 +1,14 @@
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Boolean> seen = new HashMap<Integer,Boolean>();
+        for (int i = 0; i < nums.length; i++){
+            seen.put(nums[i], seen.containsKey(nums[i]));
+        }
+        for (Integer i : seen.keySet()){
+           if (!seen.get(i)){
+               return i;
+           }
+        }
+        return 0;
+    }
+}

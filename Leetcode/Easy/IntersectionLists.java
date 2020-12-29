add48d2002894f8e58a6c79a79a3ae5b9659bee5
Nick Zullo <njz12@pitt.edu>
2020-12-29 15:42:34 -0500

    Solution

--- Leetcode/Easy/IntersectionLists.java
+++ Leetcode/Easy/IntersectionLists.java
@@ -1,15 +1,32 @@
 /**
-*	O(N^2). Can improve to O(N)
+*	O(N). Equalize the lengths then match the nodes.
+*	3N because takes N time to calculate the length, which is done twice, then N to find the intersection
 */
 public class IntersectionLists {
 	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lengthA = 0;
+        int lengthB = 0;
         for (ListNode currA = headA; currA != null; currA = currA.next){
-            for (ListNode currB = headB; currB != null; currB = currB.next){
-                if (currA == currB){
-                    return currA;
-                }   
-            }           
+            lengthA++;
+        }
+        for (ListNode currB = headB; currB != null; currB = currB.next){
+            lengthB++;
+        }
+        if (lengthA > lengthB){
+            for (int i = 0; i < lengthA-lengthB; i++){
+                headA = headA.next;
+            }
+        } else {
+            for (int i = 0; i < lengthB-lengthA; i++){
+                headB = headB.next;
+            }
+        }
+        for (ListNode currA = headA, currB = headB; currA != null && currB != null; currA = currA.next, currB = currB.next){
+            if (currA == currB){
+                return currA;
+            }
         }
         return null;
+    }
     }
 }
